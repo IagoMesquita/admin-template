a121be3f6ce48ecf3d6a3787d9aa09e071f85de2
Iago Mesquita <iagomesquitaalves@gmail.com>
2023-10-09 20:07:32 -0300

    feat: style ItemMenu

--- src/components/template/ItemMenu.tsx
+++ src/components/template/ItemMenu.tsx
@@ -1,12 +1,22 @@
+import Link from "next/link"
+
 interface ItemMenuProps {
   url: string
   text: string
   icon: any
 }
-export default function ItemMenu(props: ItemMenuProps){
+export default function ItemMenu(props: ItemMenuProps) {
   return (
-    <li>
-      {props.icon}
+    <li className={` hover:bg-gray-100`}>
+      <Link href={props.url} className={`
+        flex flex-col justify-center items-center
+        h-20 w-20
+      `}>
+          {props.icon}
+          <span className={` text-xs font-light text-gray-600`}>
+            {props.text}
+          </span>
+      </Link>
     </li>
   )
 }
