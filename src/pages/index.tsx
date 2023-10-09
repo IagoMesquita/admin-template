5971b463c6145a685b3b32c9ede8b84e6bfe0ec1
Iago Mesquita <iagomesquitaalves@gmail.com>
2023-10-09 07:00:31 -0300

    first tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,11 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div className={`bg-gradient-to-r from-green-500 to-blue-500`}>
+    <div className={`
+    flex justify-center items-center h-screen
+    bg-gradient-to-r from-green-500 to-blue-500
+    `
+    }>
       <h1>Iniciando Projeto Admin</h1>
     </div>
   )
