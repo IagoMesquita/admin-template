7c3645fed578279e80b0c7699331682f1caa292a
Iago Mesquita <iagomesquitaalves@gmail.com>
2023-10-09 19:39:47 -0300

    feat: Icons in LateralMenu

--- src/components/template/LateralMenu.tsx
+++ src/components/template/LateralMenu.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { HomeIcon, AdjustmentsIcon, BellIcon, LogoutIcon } from "../icons";
 import ItemMenu from "./ItemMenu";
 import Logo from "./Logo";
@@ -31,6 +32,18 @@ export default function LateralMenu() {
             hover:bg-red-400 hover:text-white dark:hover:text-white
           `}
         />
+=======
+import { HomeIcon, AdjustmentsIcon, BellIcon } from "../icons";
+import ItemMenu from "./ItemMenu";
+
+export default function LateralMenu() {
+  return (
+    <aside>
+      <ul>
+        <ItemMenu url="/" text="Iníco" icon={HomeIcon}/>
+        <ItemMenu url="/ajustes" text="Ajustes" icon={AdjustmentsIcon}/>
+        <ItemMenu url="/notificacoes" text="Notificações" icon={BellIcon}/>
+>>>>>>> 41e516d (feat: Icons in LateralMenu)
       </ul>
     </aside>
   )
