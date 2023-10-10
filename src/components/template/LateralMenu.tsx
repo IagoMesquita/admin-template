5c12bd95aaa9cca41e30679806172897478286f3
Iago Mesquita <iagomesquitaalves@gmail.com>
2023-10-09 21:53:13 -0300

    feat: logout visual button

--- src/components/template/LateralMenu.tsx
+++ src/components/template/LateralMenu.tsx
@@ -1,22 +1,32 @@
-import { HomeIcon, AdjustmentsIcon, BellIcon } from "../icons";
+import { HomeIcon, AdjustmentsIcon, BellIcon, LogoutIcon } from "../icons";
 import ItemMenu from "./ItemMenu";
 import Logo from "./Logo";
 
 export default function LateralMenu() {
   return (
-    <aside>
+    <aside className="flex flex-col">
       <div className={` 
         h-20 w-20  
         bg-gradient-to-r from-indigo-500 to-purple-800
         flex flex-col justify-center items-center 
-
         `}>
-          <Logo/>
-        </div>
-      <ul>
+        <Logo />
+      </div>
+      <ul className="flex-grow">
         <ItemMenu url="/" text="Iníco" icon={HomeIcon} />
         <ItemMenu url="/adjustments" text="Ajustes" icon={AdjustmentsIcon} />
         <ItemMenu url="/notifications" text="Notificações" icon={BellIcon} />
+      </ul>
+      <ul>
+        <ItemMenu
+          onClick={() => console.log("Sair!!!")}
+          text="Sair"
+          icon={LogoutIcon} 
+          className={`
+          text-red-600 
+            hover:bg-red-400 hover:text-white
+          `}
+        />
       </ul>
     </aside>
   )
