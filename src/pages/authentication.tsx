1544b34fe165c4959a6d6ef99a696ccaaa67b087
Iago Mesquita <iagomesquitaalves@gmail.com>
2023-10-12 13:28:25 -0300

    faet: finish screen login

--- src/pages/authentication.tsx
+++ src/pages/authentication.tsx
@@ -1,4 +1,5 @@
 import AuthInput from "@/components/auth/AuthInput";
+// import Image from "next/image";
 import { useState } from "react";
 
 export default function Authentication() {
@@ -16,9 +17,16 @@ export default function Authentication() {
   }
 
   return (
-    <div className="h-screen flex flex-col items-center  justify-center ">
-      <div className="w-1/2">
-        <h1 className={`text-xl font-bold mb-5`}>
+    <div className="h-screen flex  items-center  justify-center ">
+      <div className="hidden md:block md:w-1/2">
+        <img  
+          src="https://source.unsplash.com/random"
+          alt="Imagens aleatótias da tela de autenticação"
+          className=" h-screen w-full object-cover"
+        />
+      </div>
+      <div className=" m-10 w-full md:w-1/2">
+        <h1 className={`text-3xl font-bold mb-5`}>
           {mode === 'login' ? 'Entre com sua senha' : 'Cadastre-se na Plataforma'}
         </h1>
         <AuthInput
@@ -55,6 +63,35 @@ export default function Authentication() {
         >
           Entrar com Google
         </button>
+
+        {mode === 'login' ? (
+          <p className="mt-8">
+            Novo por aqui?
+            <a onClick={() => setMode('register')}
+              className={`
+                text-blue-500 hover:text-blue-700 font-semibold
+                cursor-pointer
+                pl-2
+              `}
+            >
+              Criar uma conta Gratuitamente.
+            </a>
+          </p>
+        ) :(
+          <p className="mt-8">
+          Já faz parte da nossa comunidade?
+          <a onClick={() => setMode('login')}
+            className={`
+              text-blue-500 hover:text-blue-700 font-semibold
+              cursor-pointer
+              pl-2
+            `}
+          >
+            Entre com suas Credenciais.
+          </a>
+        </p>
+        )}
+
       </div>
     </div>
 
