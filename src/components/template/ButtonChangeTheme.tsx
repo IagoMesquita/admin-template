69b12d065d4c404684eb470228dba68bfb2086fe
Iago Mesquita <iagomesquitaalves@gmail.com>
2023-10-12 10:44:42 -0300

    feat: button dark theme

--- src/components/template/ButtonChangeTheme.tsx
+++ src/components/template/ButtonChangeTheme.tsx
@@ -1,4 +1,4 @@
-import { SunIcon } from "../icons";
+import { MoonIcon, SunIcon } from "../icons";
 
 interface ButtonChangeThemeProps {
   theme?: string;
@@ -8,7 +8,7 @@ interface ButtonChangeThemeProps {
 export default function ButtonChangeTheme(props: ButtonChangeThemeProps) {
   return props.theme === 'dark' ? (
     <div onClick={props.changeTheme} className={`
-      hidden sm:flex justify-center cursor-pointer
+      sm:flex items-center cursor-pointer
       bg-gradient-to-r from-yellow-300 to-yellow-600
       w-14 lg:w-24 h-8 p-1 rounded-full
     `}>
@@ -16,18 +16,37 @@ export default function ButtonChangeTheme(props: ButtonChangeThemeProps) {
         flex items-center justify-center
         bg-white text-yellow-600 
         w-6 h-6 rounded-full 
-      `}> 
+      `}>
         {SunIcon(4)}
       </div>
       <div className={`
         hidden lg:flex items-center ml-4
         text-white 
       `}>
-        <span>Claro</span>
+        <span className="text-sm">Claro</span>
       </div>
     </div>
   ) : (
-    <div></div>
+    <div onClick={props.changeTheme} className={`
+    sm:flex items-center  justify-end cursor-pointer
+    bg-gradient-to-r from-gray-500 to-gray-900
+    w-14 lg:w-24 h-8 p-1 rounded-full
+  `}>
+      <div className={`
+      hidden  lg:flex items-center mr-2
+      text-gray-300 
+    `}>
+        <span className="text-sm">Escuro</span>
+      </div>
+      <div className={`
+      flex items-center justify-center
+      bg-black text-yellow-300 
+      w-6 h-6 rounded-full 
+    `}>
+        {MoonIcon(4)}
+      </div>
+
+    </div>
   )
 
 
