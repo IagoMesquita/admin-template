d0cd71b588b2fc07007aec2ab0143f1f0f7eeeb9
Iago Mesquita <iagomesquitaalves@gmail.com>
2023-10-12 12:38:25 -0300

    feat: style  input

--- src/components/auth/AuthInput.tsx
+++ src/components/auth/AuthInput.tsx
@@ -9,13 +9,18 @@ interface AuthInputProps {
 
 export default function AuthInput(props: AuthInputProps) {
   return (
-    <div className={`flex flex-col `}>
+    <div className={`flex flex-col mt-4`}>
       <label>{props.label}</label>
       <input
         type={props.type}
         value={props.value}
         onChange={(e) => props.changeValue?.(e.target.value)}
         required={props.required}
+        className={`
+        px-4 py-3 rounded-lg bg-gray-200 mt-2
+        border focus:border-blue-500 focus:bg-white
+        focus: outline-none
+        `}
       />
     </div>
   )
