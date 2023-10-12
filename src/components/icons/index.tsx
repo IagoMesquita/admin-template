66659888fe17fe0f273eb6db87f9dc747dba53f1
Iago Mesquita <iagomesquitaalves@gmail.com>
2023-10-12 17:33:08 -0300

    feat: Icon Warn

--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -34,3 +34,10 @@ export const MoonIcon = (size = 6) => (
     <path strokeLinecap="round" strokeLinejoin="round" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
   </svg>
 )
+
+export const WarnIcon = (size = 6) => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`h-${size} w-${size}`}>
+    <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
+  </svg>
+
+)
