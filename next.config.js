2f39736919bfa7652ee536832669788b2739ef5d
Iago Mesquita <iagomesquitaalves@gmail.com>
2023-10-16 23:14:32 -0300

    chore: domain image goole

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // images: {
-  //   remotePatterns: [
-  //     {
-  //       protocol: 'https',
-  //       hostname: 'source.unsplash.com',
-  //     },
-  //   ],
-  // },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname:  'lh3.googleusercontent.com',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
