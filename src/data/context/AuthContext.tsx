fe02b79d46da50c51ec854e0c6758a1207c45be3
Iago Mesquita <iagomesquitaalves@gmail.com>
2023-10-16 22:34:17 -0300

    fix: add trycatch and import correct firebase

--- src/data/context/AuthContext.tsx
+++ src/data/context/AuthContext.tsx
@@ -1,8 +1,7 @@
 import { createContext, useState } from 'react'
 import route from 'next/router'
 import User from '@/model/User'
-import firebase from 'firebase'
-
+import firebase from '../../firebase/config'
 
 
 interface AuthContextProps {
@@ -16,28 +15,44 @@ interface Props {
 
 const AuthContext = createContext<AuthContextProps>({})
 
-// async function normalizedUser(userFirebase: firebase.User): Promise<User> {
-//   const token = await userFirebase.getIdToken()
-//  return {
-//     uid: userFirebase.uid,
-//     name: userFirebase.displayName,
-//     email: userFirebase.email,
-//     token,
-//     provider: userFirebase.providerData[0]?.providerId,
-//     imageUrl: userFirebase.photoURL
+async function normalizedUser(userFirebase: firebase.User): Promise<User> {
+  const token = await userFirebase.getIdToken()
+  return {
+    uid: userFirebase.uid,
+    name: userFirebase!.displayName,
+    email: userFirebase!.email,
+    token,
+    provider: userFirebase.providerData[0]?.providerId,
+    imageUrl: userFirebase.photoURL
 
-//   }
-// }
+  }
+}
 
 export function AuthProvider(props: Props) {
 
   const [user, setUser] = useState<User>()
 
   async function loginGoogle() {
-    console.log('Login Google...')
-    route.push('/')
+
+    try {
+      const resp = await firebase.auth().signInWithPopup(
+        new firebase.auth.GoogleAuthProvider()
+      )
+
+      // se user vier nulo por algum motivo, "email" só será chamado se "user?" estiver diferente de nulo válido
+      if (resp.user?.email) {
+        const user = await normalizedUser(resp.user)
+        setUser(user)
+
+        route.push('/')
+      }
+    } catch (error) {
+      console.error("ERROR AQUIII", error)
+    }
+
+
   }
-  
+
 
   return (
     <AuthContext.Provider value={{
